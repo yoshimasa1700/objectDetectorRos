972c9404dce66918ad729d57f28a40b0c63e99c0
Masahiko Toyoshi <masahiko@MasaPC.(none)>
2013-11-14 04:00:27 +0900

    add message

--- src/objectDetector.cpp
+++ src/objectDetector.cpp
@@ -14,119 +14,127 @@
 
 #include <boost/filesystem.hpp>
 
+#include <objectDetector/Detect.h>
+
 namespace enc = sensor_msgs::image_encodings;
 
 class ImageConverter
 {
 public:
-  ImageConverter() : image_trans_(nh_){
+ImageConverter() : image_trans_(nh_){
     
-        std::cerr << boost::filesystem::current_path() << std::endl;
+std::cerr << boost::filesystem::current_path() << std::endl;
 
 
-    conf.loadConfig("config.xml");
-    conf.demoMode = 1;
-    conf.tsukubaMode = 1;
-    forest = NULL;
-    forest = new CRForest(conf);
-    forest->loadForest();
+conf.loadConfig("config.xml");
+conf.demoMode = 1;
+conf.tsukubaMode = 1;
+forest = NULL;
+forest = new CRForest(conf);
+forest->loadForest();
 
-    image_pub_ = image_trans_.advertise("out", 1);
-    image_sub_ = image_trans_.subscribe("image_raw", 1, &ImageConverter::image_callback, this);
+image_pub_ = image_trans_.advertise("out", 1);
+image_sub_ = image_trans_.subscribe("image_raw", 1, &ImageConverter::image_callback, this);
 
-    // init opencv windows
-    cv::namedWindow("Image window");
-    //cv::namedWindow("mask");
-    //cv::namedWindow("result");
+// init opencv windows
+cv::namedWindow("Image window");
+//cv::namedWindow("mask");
+//cv::namedWindow("result");
 
+detect_pub = nh_.advertise<objectDetector::Detect>("objectdetector1/detect",1);
 
+}
 
-  }
+~ImageConverter()
+{
+cv::destroyAllWindows();
+}
 
-  ~ImageConverter()
-  {
-    cv::destroyAllWindows();
-  }
+void image_callback(const sensor_msgs::ImageConstPtr& msg){
 
-  void image_callback(const sensor_msgs::ImageConstPtr& msg){
+cv_bridge::CvImagePtr cv_ptr;
+try{
+cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
+}
+ catch (cv_bridge::Exception& e){
+ROS_ERROR("cv_bridge exception: %s", e.what());
+return;
+}
 
-    cv_bridge::CvImagePtr cv_ptr;
-    try{
-      cv_ptr = cv_bridge::toCvCopy(msg, enc::BGR8);
-    }
-    catch (cv_bridge::Exception& e){
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
-
-    cv::Mat inputImage(cv_ptr->image);
-    // cv::Mat img_mask;
-    // cv::Mat img_bkgmodel;
-    // cv::Mat dest = cv::Mat::zeros(inputImage.rows, inputImage.cols, inputImage.type());
-    // cv::Rect bb;
+cv::Mat inputImage(cv_ptr->image);
+// cv::Mat img_mask;
+// cv::Mat img_bkgmodel;
+// cv::Mat dest = cv::Mat::zeros(inputImage.rows, inputImage.cols, inputImage.type());
+// cv::Rect bb;
   
-    CTestDataset seqImg;
-    CDetectionResult detectR;
+CTestDataset seqImg;
+CDetectionResult detectR;
 
-    seqImg.img.push_back(&inputImage);
-    detectR = forest->detection(seqImg);
+seqImg.img.push_back(&inputImage);
+detectR = forest->detection(seqImg);
+
+
+objectDetector::Detect detectResult;
+detect_pub.publish(detectResult);
 
     
-    cv::imshow("Image window", inputImage);	
+cv::imshow("Image window", inputImage);	
     
 	
-    int key = cv::waitKey(1);
+int key = cv::waitKey(1);
 
-    switch(key){
-    case 'q':
-      exit(0);
-      break;
+switch(key){
+ case 'q':
+exit(0);
+break;
 	  
-    // case 't':
-    //   cv::imwrite("test.png", inputImage);
-    //   break;
+// case 't':
+//   cv::imwrite("test.png", inputImage);
+//   break;
 
-    // case 's':
-    //   takeFlag = 1;
-    //   break;
+// case 's':
+//   takeFlag = 1;
+//   break;
 
-    // case 'p':
-    //   takeFlag = 0;
-    //   break;
+// case 'p':
+//   takeFlag = 0;
+//   break;
 
-    defalut:
-       break;
+defalut:
+break;
 	
-    }
-    //ここまで処理
+}
+//ここまで処理
 
-    //処理後の画像をpublish
-    image_pub_.publish(cv_ptr->toImageMsg());
-  }
+//処理後の画像をpublish
+image_pub_.publish(cv_ptr->toImageMsg());
+}
 
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport image_trans_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
+ros::NodeHandle nh_;
+image_transport::ImageTransport image_trans_;
+image_transport::Subscriber image_sub_;
+image_transport::Publisher image_pub_;
 
-  CRForest *forest;
-  CConfig conf;
+ros::Publisher detect_pub;
 
-  // IBGS *bgs;
-  // cv::Mat bg;
-  // int imageNum;
-  // int takeFlag;
-  // std::ofstream dl;
+CRForest *forest;
+CConfig conf;
+
+// IBGS *bgs;
+// cv::Mat bg;
+// int imageNum;
+// int takeFlag;
+// std::ofstream dl;
 
 };
 
 int main(int argc, char** argv){
 
 
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
-  ros::spin();
-  return 0;
+ros::init(argc, argv, "image_converter");
+ImageConverter ic;
+ros::spin();
+return 0;
 }
 
 
